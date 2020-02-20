ba3516fc1eabe1fc693a17a6776e2ba2c6e78691
global_name <global_email>
2020-02-20 18:58:03 +0800

    '完成了Home页面的轮播图模块和推荐模块'

---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" active-color="blue">
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/home.svg">
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/home_active.svg">
+      </template>
+      <template v-slot:item-text>
+        <div>主页</div>
+      </template>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/category.svg">
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/category_active.svg">
+      </template>
+      <template v-slot:item-text>
+        <div>分类</div>
+      </template>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/shopcart.svg">
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/shopcart_active.svg">
+      </template>
+      <template v-slot:item-text>
+        <div>购物车</div>
+      </template>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/profile.svg">
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/profile_active.svg">
+      </template>
+      <template v-slot:item-text>
+        <div>我的</div>
+      </template>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from "components/tabbar/TabBar";
+  import TabBarItem from "components/tabbar/TabBarItem";
+
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBarItem,
+      TabBar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
